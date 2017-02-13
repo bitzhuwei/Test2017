f9077929daa540d47f951baf080f48b15df86de1
祝威 <bitzhuwei.cnblogs@qq.com>
2017-02-13 16:32:16 +0800

    fix: Editor attibute error.

--- EMGraphics/Scene/ViewPort.ILayout.cs
+++ EMGraphics/Scene/ViewPort.ILayout.cs
@@ -137,8 +137,8 @@ namespace EMGraphics
         ///
         /// </summary>
         [Category(strTreeNode)]
-        [Editor(typeof(IListEditor<ViewPort>), typeof(UITypeEditor))]
         [Description("Children UI Renderers.")]
+        [Editor(typeof(IListEditor<ITreeNode<ViewPort>>), typeof(UITypeEditor))]
         public ChildList<ViewPort> Children { get; private set; }
     }
 }
