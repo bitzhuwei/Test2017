6233ae0f4c047415ac95e1dde7c6aca0755d03d6
祝威 <bitzhuwei.cnblogs@qq.com>
2017-03-13 16:10:44 +0800

    order normal line

---
+++ EMGraphics/EM/FaceNormalRenderer.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace EMGraphics
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class NormalLineRenderer : PickableRenderer
+    {
+        public MarkableStruct<Color> HeadColor { get; set; }
+        private long headColorUpdatedTicks;
+
+        public MarkableStruct<Color> TailColor { get; set; }
+        private long tailColorUpdatedTicks;
+
+		private static IShaderProgramProvider provider;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static NormalLineRenderer Create(NormalLineModel model)
+        {
+			if (provider == null)
+			{
+				var shaderCodes = new ShaderCode[2];
+				shaderCodes[0] = new ShaderCode(ManifestResourceLoader.LoadTextFile(@"EM\shaders\NormalLine.vert"), ShaderType.VertexShader);
+				shaderCodes[1] = new ShaderCode(ManifestResourceLoader.LoadTextFile(@"EM\shaders\NormalLine.frag"), ShaderType.FragmentShader);
+				provider = new ShaderCodeArray(shaderCodes);
+			}
+            var map = new AttributeMap();
+            map.Add("inPosition", NormalLineModel.strPosition);
+            var renderer = new NormalLineRenderer(model, provider, map, EMGrid.strPosition);
+            renderer.ModelSize = model.ModelSize;
+            renderer.WorldPosition = model.WorldPosition;
+            renderer.RotationAngleDegree = model.RotationAngleDegree;
+            renderer.RotationAxis = model.RotationAxis;
+            renderer.Scale = model.Scale;
+
+            return renderer;
+        }
+
+		private NormalLineRenderer(IBufferable model, IShaderProgramProvider shaderProgramProvider,
+			AttributeMap attributeMap, string positionNameInIBufferable,
+			params GLState[] switches)
+			: base(model, shaderProgramProvider, attributeMap, positionNameInIBufferable, switches)
+		{
+			this.HeadColor = new MarkableStruct<Color>(Color.FromArgb(255, 100, 150, 150));
+			this.TailColor = new MarkableStruct<Color>(Color.Red);
+			this.StateList.Add(new LineWidthState(1));
+		}
+
+		protected override void DoRender(RenderEventArgs arg)
+        {
+            mat4 projection = arg.Camera.GetProjectionMatrix();
+            mat4 view = arg.Camera.GetViewMatrix();
+            mat4 model = this.GetModelMatrix().Value;
+            this.SetUniform("mvpMatrix", projection * view * model);
+            if (this.HeadColor.UpdateTicks != this.headColorUpdatedTicks)
+            {
+                this.SetUniform("headColor", this.HeadColor.Value.ToVec3());
+                this.headColorUpdatedTicks = this.HeadColor.UpdateTicks;
+            }
+            if (this.TailColor.UpdateTicks != this.tailColorUpdatedTicks)
+            {
+                this.SetUniform("tailColor", this.TailColor.Value.ToVec3());
+                this.tailColorUpdatedTicks = this.TailColor.UpdateTicks;
+            }
+
+            base.DoRender(arg);
+        }
+
+		/// <summary>
+		/// 反转法线方向。
+		/// <para>Reverse normal's direction.</para>
+		/// </summary>
+		public void ReverseNormals()
+		{
+			VertexBuffer positionBuffer = this.PositionBuffer;
+
+			IntPtr pointer = positionBuffer.MapBuffer(MapBufferAccess.ReadWrite);
+			unsafe
+			{
+				vec3* array = (vec3*)pointer.ToPointer();
+				for (int i = 0; i < positionBuffer.Length/4; i++)
+				{
+					vec3 head = array[i * 4 + 0];
+					vec3 part = array[i * 4 + 1];
+					vec3 tail = array[i * 4 + 3];
+					vec3 newTail = head + head - tail;
+					vec3 newPart = head + head - part;
+					array[i * 4 + 1] = newPart;
+					array[i * 4 + 2] = newPart;
+					array[i * 4 + 3] = newTail;
+				}
+			}
+			positionBuffer.UnmapBuffer();
+		}
+    }
+}
