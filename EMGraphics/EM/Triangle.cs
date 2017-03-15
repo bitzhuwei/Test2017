9e0c36802eb126bd4cf1406eaf0b8d9e094d32af
祝威 <bitzhuwei.cnblogs@qq.com>
2017-03-16 00:43:45 +0800

    update

--- EMGraphics/EM/Triangle.cs
+++ EMGraphics/EM/Triangle.cs
@@ -5,48 +5,48 @@ using System.Text;
 
 namespace EMGraphics
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    public class Triangle
-    {
-        /// <summary>
-        /// 本面元的第一个点
-        /// </summary>
-        public int Num1 { get; private set; }
+	/// <summary>
+	/// 
+	/// </summary>
+	public class Triangle
+	{
+		/// <summary>
+		/// 本面元的第一个点
+		/// </summary>
+		public int Num1 { get; private set; }
 
-        /// <summary>
-        /// 本面元的第二个点
-        /// </summary>
-        public int Num2 { get; private set; }
+		/// <summary>
+		/// 本面元的第二个点
+		/// </summary>
+		public int Num2 { get; private set; }
 
-        /// <summary>
-        /// 本面元的第三个点
-        /// </summary>
-        public int Num3 { get;private set; }
+		/// <summary>
+		/// 本面元的第三个点
+		/// </summary>
+		public int Num3 { get; private set; }
 
-        /// <summary>
-        /// 网格的标签
-        /// </summary>
-        public string FaceLabel { get; private set; }
+		/// <summary>
+		/// 网格的标签
+		/// </summary>
+		public string FaceLabel { get; private set; }
 
 		/// <summary>
 		/// Hashcode of <see cref="FaceLabel"/>.
 		/// </summary>
 		internal int LabelHash { get; private set; }
 
-        public Triangle(int num1, int num2, int num3, string faceLabel)
-        {
-            this.Num1 = num1;
-            this.Num2 = num2;
-            this.Num3 = num3;
-            this.FaceLabel = faceLabel;
+		public Triangle(int num1, int num2, int num3, string faceLabel)
+		{
+			this.Num1 = num1;
+			this.Num2 = num2;
+			this.Num3 = num3;
+			this.FaceLabel = faceLabel;
 			this.LabelHash = faceLabel.GetHashCode();
-        }
+		}
 
-        public override string ToString()
-        {
-            return string.Format("{0}, {1}, {2}, label: {3}", this.Num1, this.Num2, this.Num3, this.FaceLabel);
-        }
-    }
+		public override string ToString()
+		{
+			return string.Format("{0}, {1}, {2}, label: {3}", this.Num1, this.Num2, this.Num3, this.FaceLabel);
+		}
+	}
 }
