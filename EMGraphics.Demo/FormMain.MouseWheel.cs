7ec102198220220165c51e1d76079318529ec8a1
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-04-03 00:46:29 +0800

    scale

--- EMGraphics.Demo/FormMain.MouseWheel.cs
+++ EMGraphics.Demo/FormMain.MouseWheel.cs
@@ -22,7 +22,23 @@ namespace EMGraphics.Demo
         {
             ICanvas canvas = sender as ICanvas;
             Point mousePosition = e.Location;
-            ScaleObject(e);
+            Size size = canvas.ClientRectangle.Size;
+            float leftPercent = (float)(mousePosition.X - 0) / (float)size.Width;
+            float topPercent = (float)(mousePosition.Y - 0) / (float)size.Height;
+            double factor = 0.001 * e.Delta;
+            IOrthoViewCamera camera = this.camera;
+            double left = camera.Left;
+            double right = camera.Right;
+            double bottom = camera.Bottom;
+            double top = camera.Top;
+            double width = right - left;
+            double height = top - bottom;
+            camera.Left = left + width * leftPercent * factor;
+            camera.Right = right - width * (1 - leftPercent) * factor;
+            camera.Bottom = bottom + height * (1 - topPercent) * factor;
+            camera.Top = top - height * topPercent * factor;
+
+            //ScaleObject(e);
         }
 
         private void ScaleObject(MouseEventArgs e)
