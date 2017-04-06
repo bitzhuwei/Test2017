646443259ea2012d785f3cb405028f8d8df5545d
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-04-06 14:42:23 +0800

    sc

--- EMGraphics/0Foundations/OpenGLObjects/Camera/ICamera.Resize.cs
+++ EMGraphics/0Foundations/OpenGLObjects/Camera/ICamera.Resize.cs
@@ -12,37 +12,61 @@ namespace EMGraphics
         /// <param name="currentSize">canvas' current size.</param>
         public static void Resize(this ICamera camera, Size lastSize, Size currentSize)
         {
-            IPerspectiveCamera perspectiveCamera = camera;
-            double lastAspectRatio = perspectiveCamera.AspectRatio;
 
             // update perspective camera.
             {
+                IPerspectiveCamera perspectiveCamera = camera;
                 perspectiveCamera.AspectRatio = ((double)currentSize.Width) / ((double)currentSize.Height);
             }
 
             // update ortho camera.
             {
                 IOrthoCamera orthoCamera = camera;
-                if (lastSize.Width != currentSize.Width)
-                {
-                    double lastWidth = orthoCamera.Right - orthoCamera.Left;
-                    double widthRatio = ((double)currentSize.Width) / ((double)lastSize.Width);
-                    double currentWidth = lastWidth * widthRatio;
-                    double center = (orthoCamera.Left + orthoCamera.Right) / 2.0 * widthRatio;
-                    orthoCamera.Left = center - currentWidth / 2.0;
-                    orthoCamera.Right = center + currentWidth / 2.0;
-                }
+                //// fit window size.
+                //if (lastSize.Width != currentSize.Width)
+                //{
+                //    double lastWidth = orthoCamera.Right - orthoCamera.Left;
+                //    double widthRatio = ((double)currentSize.Width) / ((double)lastSize.Width);
+                //    double currentWidth = lastWidth * widthRatio;
+                //    double center = (orthoCamera.Left + orthoCamera.Right) / 2.0 * widthRatio;
+                //    orthoCamera.Left = center - currentWidth / 2.0;
+                //    orthoCamera.Right = center + currentWidth / 2.0;
+                //}
+                //// fit window size.
+                //if (lastSize.Height != currentSize.Height)
+                //{
+                //    double lastHeight = orthoCamera.Top - orthoCamera.Bottom;
+                //    double heightRatio = ((double)currentSize.Height) / ((double)lastSize.Height);
+                //    double currentHeight = lastHeight * heightRatio;
+                //    double center = (orthoCamera.Bottom + orthoCamera.Top) / 2.0 * heightRatio;
+                //    orthoCamera.Bottom = center - currentHeight / 2.0;
+                //    orthoCamera.Top = center + currentHeight / 2.0;
+                //}
 
-                if (lastSize.Height != currentSize.Height)
+                // scale scene.
+                if (currentSize.Width >= currentSize.Height)
                 {
-                    double lastHeight = orthoCamera.Top - orthoCamera.Bottom;
-                    double heightRatio = ((double)currentSize.Height) / ((double)lastSize.Height);
-                    double currentHeight = lastHeight * heightRatio;
-                    double center = (orthoCamera.Bottom + orthoCamera.Top) / 2.0 * heightRatio;
-                    orthoCamera.Bottom = center - currentHeight / 2.0;
-                    orthoCamera.Top = center + currentHeight / 2.0;
+                    double ratio1 = (double)currentSize.Width / (double)lastSize.Width;
+                    double ratio2 = (double)lastSize.Height / (double)currentSize.Height;
+                    double currentWidth = orthoCamera.Right - orthoCamera.Left;
+                    double center = (orthoCamera.Right + orthoCamera.Left) / 2.0;
+                    double newWidth = currentWidth * ratio1 * ratio2;
+                    orthoCamera.Left = center - newWidth / 2;
+                    orthoCamera.Right = center + newWidth / 2;
                 }
-                //if (aspectRatio > lastAspectRatio)
+                else
+                {
+                    double ratio1 = (double)currentSize.Height / (double)lastSize.Height;
+                    double ratio2 = (double)lastSize.Width / (double)currentSize.Width;
+                    double currentHeight = orthoCamera.Top - orthoCamera.Bottom;
+                    double center = (orthoCamera.Top + orthoCamera.Bottom) / 2;
+                    double newHeight = currentHeight * ratio1 * ratio2;
+                    orthoCamera.Bottom = center - newHeight / 2;
+                    orthoCamera.Top = center + newHeight / 2;
+                }
+                //double aspect = (double)currentSize.Width / (double)currentSize.Height;
+                //double lastAspect = (double)lastSize.Width / (double)lastSize.Height;
+                //if (aspect > lastAspect)
                 //{
                 //    double top = orthoCamera.Top;
                 //    double newRight = top * aspectRatio;
