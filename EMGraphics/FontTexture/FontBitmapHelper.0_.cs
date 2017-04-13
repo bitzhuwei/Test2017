7ceeccd53f32952890c90a7dd7346bfc133de349
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-04-14 01:31:49 +0800

    new version of

--- EMGraphics/FontTexture/FontBitmapHelper.0_.cs
+++ EMGraphics/FontTexture/FontBitmapHelper.0_.cs
@@ -22,145 +22,21 @@ namespace EMGraphics
         /// <summary>
         /// Gets a <see cref="FontBitmap"/>'s intance.
         /// </summary>
-        /// <param name="font"></param>
+        /// <param name="font">建议最大字体不超过32像素高度，否则可能无法承载所有Unicode字符。</param>
         /// <param name="charSet"></param>
         /// <param name="drawBoundary"></param>
         /// <returns></returns>
         public static FontBitmap GetFontBitmap(this Font font, string charSet, bool drawBoundary = false)
         {
             var fontBitmap = new FontBitmap();
-            fontBitmap.GlyphFont = font;
+            fontBitmap.GlyphFont = font; //fontBitmap.GlyphInfoDictionary //fontBitmap.GlyphHeight //fontBitmap.GlyphBitmap
 
-            Size charSize = GetSingleCharSize(charSet[0], font);
+            fontBitmap.GlyphInfoDictionary = GetGlyphDict(font, charSet);
+            int textureWidth = GetTextureWidth(fontBitmap.GlyphInfoDictionary);
+            fontBitmap.GlyphHeight = GetGlyphHeight(fontBitmap.GlyphInfoDictionary, textureWidth);
+            int textureHeight = LayoutGlyphs(fontBitmap.GlyphInfoDictionary, textureWidth, fontBitmap.GlyphHeight);
+            fontBitmap.GlyphBitmap = PaintTexture(textureWidth, textureHeight, fontBitmap.GlyphInfoDictionary, font);
 
-            fontBitmap.GlyphHeight = charSize.Height;
-
-            var bitmap = new Bitmap(charSize.Width * charSet.Length, charSize.Height);
-            using (var graphics = Graphics.FromImage(bitmap))
-            {
-                Color clearColor = Color.FromArgb(0, 0, 0, 0);
-                graphics.Clear(clearColor);
-                int index = 0;
-                foreach (var item in charSet)
-                {
-                    using (var glyphBmp = new Bitmap(charSize.Width, charSize.Height))
-                    {
-                        using (var glyphGraphics = Graphics.FromImage(glyphBmp))
-                        {
-                            glyphGraphics.DrawString(item.ToString(), font, Brushes.Red, 0, 0);
-                        }
-                        graphics.DrawImage(glyphBmp, index * charSize.Width, 0);
-                        index++;
-                    }
-                }
-            }
-            fontBitmap.GlyphBitmap = bitmap;
-            var dict = new FullDictionary<char, GlyphInfo>(GlyphInfo.Default);
-            for (int i = 0; i < charSet.Length; i++)
-            {
-                char c = charSet[i];
-                var glyphInfo = new GlyphInfo(
-                    i * charSize.Width + charSize.Width / 5, 0, charSize.Width * 3 / 5, charSize.Height);
-                dict.Add(c, glyphInfo);
-            }
-            fontBitmap.GlyphInfoDictionary = dict;
-
-            return fontBitmap;
-        }
-
-        private static Size GetSingleCharSize(char c, Font font)
-        {
-            using (var bitmap = new Bitmap(1, 1))
-            {
-                using (var graphics = Graphics.FromImage(bitmap))
-                {
-                    SizeF size = graphics.MeasureString(c.ToString(), font);
-                    return new Size((int)Math.Ceiling(size.Width), (int)Math.Ceiling(size.Height));
-                }
-            }
-        }
-
-        private static void PrintSingleChars(Font font, string charSet)
-        {
-            int index = 0;
-            foreach (var item in charSet)
-            {
-                SizeF size;
-                using (var bitmap = new Bitmap(1, 1))
-                {
-                    using (var graphics = Graphics.FromImage(bitmap))
-                    {
-                        size = graphics.MeasureString(item.ToString(), font);
-                    }
-                }
-                using (var bitmap = new Bitmap((int)Math.Ceiling(size.Width), (int)Math.Ceiling(size.Height)))
-                {
-                    using (var graphics = Graphics.FromImage(bitmap))
-                    {
-                        graphics.DrawString(item.ToString(), font, Brushes.Red, 0, 0);
-                    }
-                    bitmap.Save(string.Format("{2}, {0}x{1}.png", bitmap.Width, bitmap.Height, index++));
-                }
-            }
-        }
-
-        private static int GetGlyphHeight(string charSet, Font font)
-        {
-            using (var bitmap = new Bitmap(1, 1))
-            {
-                using (var graphics = Graphics.FromImage(bitmap))
-                {
-                    SizeF size = graphics.MeasureString(charSet, font);
-                    return (int)Math.Ceiling(size.Height);
-                }
-            }
-        }
-
-        private static string GetVerticalCharSet(string charSet)
-        {
-            var builder = new StringBuilder();
-            builder.Append("祝");
-            builder.AppendLine();
-            foreach (var item in charSet)
-            {
-                builder.Append(item);
-                builder.AppendLine();
-            }
-            builder.Append("威");
-            builder.AppendLine();
-
-            return builder.ToString();
-        }
-
-        private static SizeF GetStringSize(string charSet, Font font)
-        {
-            using (var bitmap = new Bitmap(1, 1))
-            {
-                using (var graphics = Graphics.FromImage(bitmap))
-                {
-                    return graphics.MeasureString(charSet, font);
-                }
-            }
-        }
-        /// <summary>
-        /// Gets a <see cref="FontBitmap"/>'s intance.
-        /// </summary>
-        /// <param name="font"></param>
-        /// <param name="charSet"></param>
-        /// <param name="drawBoundary"></param>
-        /// <returns></returns>
-        public static FontBitmap GetFontBitmap_old(this Font font, string charSet, bool drawBoundary = false)
-        {
-            var fontBitmap = new FontBitmap();
-            fontBitmap.GlyphFont = font;
-
-            // 以下几步，不能调换先后顺序。
-            // Don't change the order in which these functions invoked.
-            int singleCharWidth, singleCharHeight;
-            PrepareInitialGlyphDict(fontBitmap, charSet, out singleCharWidth, out singleCharHeight);
-            int width, height;
-            PrepareFinalBitmapSize(fontBitmap, out width, out height);
-            PrintBitmap(fontBitmap, singleCharWidth, singleCharHeight, width, height);
             if (drawBoundary)
             {
                 using (var graphics = Graphics.FromImage(fontBitmap.GlyphBitmap))
@@ -176,7 +52,106 @@ namespace EMGraphics
                     graphics.DrawRectangle(Pens.Red, 0, 0, fontBitmap.GlyphBitmap.Width - 1, fontBitmap.GlyphBitmap.Height - 1);
                 }
             }
+            //fontBitmap.GlyphBitmap.Save("aaaaaaaaaaaaa.png");
             return fontBitmap;
+        }
+
+
+        private static Bitmap PaintTexture(int textureWidth, int textureHeight, FullDictionary<char, GlyphInfo> fullDictionary, Font font)
+        {
+            var bitmap = new Bitmap(textureWidth, textureHeight);
+            using (var graphics = Graphics.FromImage(bitmap))
+            {
+                foreach (var item in fullDictionary)
+                {
+                    Size oneSize = graphics.MeasureString(string.Format("{0}", item.Key), font).ToSize();
+
+                    using (var oneBitmap = new Bitmap(oneSize.Width, oneSize.Height))
+                    {
+                        using (var oneGrphics = Graphics.FromImage(oneBitmap))
+                        { oneGrphics.DrawString(string.Format("{0}", item.Key), font, Brushes.Red, 0, 0); }
+
+                        graphics.DrawImage(oneBitmap,
+                            item.Value.ToRectangle(),
+                            new Rectangle((oneSize.Width - item.Value.width) / 2, 0, item.Value.width, item.Value.height),
+                            GraphicsUnit.Pixel);
+                    }
+                }
+            }
+
+            return bitmap;
+        }
+
+        private static int LayoutGlyphs(FullDictionary<char, GlyphInfo> fullDictionary, int textureWidth, int glyphHeight)
+        {
+            int cursorX = 0, cursorY = 0;
+            foreach (var item in fullDictionary)
+            {
+                int width = item.Value.width;
+                int height = item.Value.height;
+                if (cursorX + width > textureWidth)
+                {
+                    cursorX = 0;
+                    cursorY += glyphHeight;
+                }
+
+                item.Value.xoffset = cursorX;
+                item.Value.yoffset = cursorY;
+
+                cursorX += width;
+            }
+
+            return cursorY + glyphHeight;
+        }
+
+        private static int GetGlyphHeight(FullDictionary<char, GlyphInfo> fullDictionary, int textureWidth)
+        {
+            int glyphHeight = 0;
+            foreach (var item in fullDictionary)
+            {
+                int height = item.Value.height;
+                if (glyphHeight < height) { glyphHeight = height; }
+            }
+
+            return glyphHeight;
+        }
+
+        private static int GetTextureWidth(FullDictionary<char, GlyphInfo> fullDictionary)
+        {
+            int totalAera = 0;
+            foreach (var item in fullDictionary)
+            {
+                int area = item.Value.width * item.Value.height;
+                totalAera += area;
+            }
+
+            return (int)Math.Sqrt(totalAera);
+        }
+
+        private static FullDictionary<char, GlyphInfo> GetGlyphDict(Font font, string charSet)
+        {
+            var dict = new FullDictionary<char, GlyphInfo>(GlyphInfo.Default);
+            using (var bmp = new Bitmap(1, 1))
+            {
+                using (var graphics = Graphics.FromImage(bmp))
+                {
+                    foreach (var c in charSet)
+                    {
+                        Size oneSize = graphics.MeasureString(string.Format("{0}", c), font).ToSize();
+                        Size doubleSize = graphics.MeasureString(string.Format("{0}{0}", c), font).ToSize();
+
+                        // TODO: Log this.
+                        if (oneSize.Height != doubleSize.Height) { continue; }
+                        if (oneSize.Width >= doubleSize.Width) { continue; }
+
+                        Size charSize = new Size(doubleSize.Width - oneSize.Width, oneSize.Height);
+                        var info = new GlyphInfo(0, 0, charSize.Width, charSize.Height);
+                        dict.Add(c, info);
+                    }
+                }
+            }
+
+            return dict;
         }
     }
 }
